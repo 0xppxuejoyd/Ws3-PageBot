852855136a728bfa2ecd945840c61899b823eb5c
Dotaka123 <142669125+Dotaka123@users.noreply.github.com>
2024-10-18 19:00:37 +0300

    Update about.js

--- ws3/commands/about.js
+++ ws3/commands/about.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: "What is WieAI?",
+  description: "What is Tata bot?",
   async run({ api, send, admin }){
     await send({
       attachment: {
@@ -15,26 +15,26 @@ module.exports = {
         type: "template",
         payload: {
           template_type: "button",
-          text: `🤖 About WieAI:
-WieAI is your friendly, helpful personal assistant.
+          text: `🤖 About Tata bot:
+Tata bot is your friendly, helpful personal assistant.
 
-💭 Why I named WieAI as a name of the page bot because this is dedicated to my girlfriend itself 👸
+💭 Why I named Tata bot as a name of the page bot because this is my nickname at class 
 
 ❓ Contact us admins if you experienced/encountered any issue regarding to the bot and I will try to fix it. Thankyou for using me as a personal assistant!`,
           buttons: [
             {
               type: "web_url",
-              url: "https://www.facebook.com/profile.php?id=61566907376981",
+              url: "https://www.facebook.com/profile.php?id=61552168730358",
               title: "Like/Follow our Page"
                 },
             {
               type: "web_url",
-              url: "https://www.facebook.com/kennethfranciscoaceberos",
+              url: "https://www.facebook.com/lahatra.gameur",
               title: "Contact Admin 1"
                 },
             {
               type: "web_url",
-              url: "https://www.facebook.com/wieginesalpocialechavez",
+              url: "https://www.facebook.com/sayah.randii",
               title: "Contact Admin 2"
                 }
              ]
